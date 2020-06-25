5eaad4e81ac74c694ab797433a453195e8dabc7b
SaraSaid-hub <66921329+SaraSaid-hub@users.noreply.github.com>
2020-06-25 17:58:35 +0200

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -12,11 +12,26 @@ using std::to_string;
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
   
-  
     int hours=seconds/3600;
     seconds=seconds%3600;
     int mins=seconds/60;
-    int secs=seconds%60;
-    
-    return to_string(hours)+ ':'+to_string(mins)+':'+to_string(secs);
+    int secs= seconds%60;
+  
+    string H,M,S;
+    H=to_string(hours);
+    M=to_string(mins);
+    S=to_string(secs);
+
+    if (hours<10 && mins<10 && secs<10){
+       return "0"+ to_string(hours)+ ':'+"0"+to_string(mins)+':'+"0"+to_string(secs);
+    }
+     if(hours<10){
+      H= "0"+to_string(hours);
+    } if(mins<10){
+      M="0"+to_string(mins);
+    } if (secs<10){
+      S="0"+to_string(secs);
+    }
+
+    return H + ':'+ M +':'+ S; 
   }
